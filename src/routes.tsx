1b37158f23485e9bbbf58fceb147a98a744d3cd1
Kleiton Marques <kleitonm.marques@gmail.com>
2020-06-22 17:11:24 -0300

    Includ redirect on menu top

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Redirect } from 'react-router-dom'
 
 import Home from './pages/home/Home'
 import Products from './pages/products/Products'
@@ -9,6 +9,7 @@ const Routes = () => {
         <BrowserRouter>
             <Route component={Home} path="/" exact />
             <Route component={Products} path="/products" />
+            <Redirect from="*" to="/" />
         </BrowserRouter>
     )
 }
