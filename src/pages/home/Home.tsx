758e812ca99cbffe3a9ead94b5b08d9a8856c666
Kleiton Marques <kleitonm.marques@gmail.com>
2020-06-25 19:32:25 -0300

    Add lib map with sucess

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import Header from '../../shared/header/Header'
 
@@ -9,7 +10,12 @@ const Home = () => {
         <div className="app-body">
             <Header />
             <div className="app-map">
-                test
+                <Map center={[-23.5120569, -46.9522795]} zoom={15}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                </Map>
             </div>
             
             
