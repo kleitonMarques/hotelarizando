ee59a2b08193118997695ed5c9ae803f793d9b7d
Kleiton Marques <kleitonm.marques@gmail.com>
2020-07-21 18:35:51 -0300

    Populating UFs side menu

--- src/shared/sideMenu/SideMenu.tsx
+++ src/shared/sideMenu/SideMenu.tsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import './sideMenu.css'
 
+interface IBGEUFResponse {
+    nome: string;
+}
+
 const SideMenu = () => {
+    const [menuUf, setMenuUf] = useState<string[]>([])
+
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.nome)
+            setMenuUf(ufInitials)
+        })
+    },[])
+
     return (
         <div className="app-side-menu">
             <div className="box-side-menu">
                 <div className="group-box-menu">
-                    <div className="listContentMenu">
+                    {menuUf.map((menuUf: string) => (
+                        <div key={menuUf} className="listContentMenu">
+                            {menuUf}
+                            <span>
+                                Capital
+                            </span>
+                        </div>
+                    ))}
+
+                    {/* <div className="listContentMenu">
                         São Paulo
                         <span>
                             São Paulo
                         </span>
-                    </div>
-                    <div className="listContentMenu">
-                        Rio de Janeiro
-                        <span>
-                            Rio de Janeiro
-                        </span>
-                    </div>
-                    <div className="listContentMenu">
-                        Paraná
-                    </div>
-                    <div className="listContentMenu">
-                        Bahia
-                    </div>
+                    </div> */}
+
                 </div>
             </div>
         </div>
